e9f5693ff5e7e6bd3bada9ddc986b76aa4d76670
ernie-hao <pdphiro@gmail.com>
2018-08-05 18:35:09 -0400

    eh: Added initial templates for homepage/ registration/profile

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Homepage from './containers/Homepage'
 
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
   render() {
-    return (<div className="container-fluid bg-light pb-5 pt-3">
+    return (
+      <Router>
+      <div class=''>
+        <Homepage />
+      </div>
+    </Router>
 
-      <h1>
-        Kooker</h1>
-    </div>);
+  );
   }
 }
 
