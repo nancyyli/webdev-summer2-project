1197883cb6f8185dbbb83e1e6ecc4ef3a626411b
ernie-hao <pdphiro@gmail.com>
2018-08-10 18:24:59 -0400

    Profile page

--- src/index.js
+++ src/index.js
@@ -1,67 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider, connect } from 'react-redux';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider, connect} from 'react-redux';
+import store from 'store';
+import {queryLoginStatus} from 'store/actions';
 import Homepage from 'containers/Homepage'
 
-import store from 'store';
-
-import { queryLoginStatus } from 'store/actions';
-
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import 'typeface-roboto'
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-
 class App extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     window.fbAsyncInit = () => {
-        window.FB.init({
-          appId            : '1734361039950244',
-          autoLogAppEvents : true,
-          xfbml            : true,
-          version          : 'v3.1'
-        });
-        window.FB.Event.subscribe('auth.statusChange', this.props.fbLoggedIn);
+      window.FB.init({appId: '1734361039950244', autoLogAppEvents: true, xfbml: true, version: 'v3.1'});
+      window.FB.Event.subscribe('auth.statusChange', this.props.fbLoggedIn);
     };
 
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-     
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     this.props.checkLoginStatus();
   }
 
-  render () {
-    return (
-      <Homepage />
-    );
+  render() {
+    return (<Homepage/>);
   }
 }
+
+const kookerTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#f04d22'
+    },
+    secondary: {
+     main: '#e91e63',
+   },
+    text: {
+      primary: '#f04d22'
+    },
+  }
+});
 
 const mapActionsToProps = dispatch => ({
   checkLoginStatus: () => dispatch(queryLoginStatus())
 });
 
-const mapStateToProps = state => ({
-
-});
+const mapStateToProps = state => ({});
 
 const ConnectedApp = connect(mapStateToProps, mapActionsToProps)(App);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Route component={ConnectedApp} />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Provider store={store}>
+  <Router>
+    <MuiThemeProvider theme={kookerTheme}>
+      <Route component={ConnectedApp}/>
+    </MuiThemeProvider>
+  </Router>
+</Provider>, document.getElementById('root'));
 
 export default App;
