dc2e8178a9f2026e7479ca83567ebc229eab086f
ernie-hao <pdphiro@gmail.com>
2018-08-19 17:51:50 -0400

    Sign in styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Typography } from '@material-ui/core';
 
 import * as actions from 'store/actions';
 
 
 class Login extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = { username: "", password: "" };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.refreshFacebook();
   }
 
@@ -32,36 +32,60 @@ class Login extends React.Component {
     });
   }
 
-  render () {
+  render() {
     return (
       <div>
         <div className="container-fluid mt-5 mb-5">
-          <div>
-            <TextField
-              id="email"
-              label="Email"
-              value={this.state.username}
-              onChange={ev => this.setState({ username: ev.target.value })}
-              margin="normal"
-              fullWidth={true}
-              required={true} />
+          <div className='row'>
+            <div className='col'>
+              <Typography variant='display1'>
+                Sign In
+          </Typography>
+            </div>
           </div>
-          <div>
-            <TextField
-              id="password"
-              label="Password"
-              type="password"
-              value={this.state.password}
-              onChange={ev => this.setState({ password: ev.target.value })}
-              margin="normal"
-              fullWidth={true}
-              required={true} />
+          <div className='row'>
+            <div className='col'>
+              <TextField
+                id="email"
+                label="Email"
+                value={this.state.username}
+                onChange={ev => this.setState({ username: ev.target.value })}
+                margin="normal"
+                fullWidth={true}
+                required={true} />
+            </div>
           </div>
-          <div>
-            <Button variant="contained" color="primary" fullWidth={true} onClick={this.handleLogin}>
-              Sign In
+          <div className='row'>
+            <div className='col'>
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                value={this.state.password}
+                onChange={ev => this.setState({ password: ev.target.value })}
+                margin="normal"
+                fullWidth={true}
+                required={true} />
+            </div>
+          </div>
+          <div className='row mt-3'>
+            <div className='col'>
+              &nbsp;
+          </div>
+            <div className='col-8'>
+              <Button variant="contained" color="primary" fullWidth={true} onClick={this.handleLogin}>
+                Sign In
             </Button>
+            </div>
+            <div className='col'>
+              &nbsp;
           </div>
+          </div>
+        </div>
+        <div className='row d-flex justify-content-center'>
+          <Typography variant='body2'>
+            Or login with loginFacebook
+        </Typography>
         </div>
         <div className="container-fluid mt-5 mb-5">
           <div className="text-center">
@@ -75,6 +99,7 @@ class Login extends React.Component {
               data-use-continue-as="true"></div>
           </div>
         </div>
+
       </div>
     );
   }
--- src/containers/RecipeDetailLayout.js
+++ src/containers/RecipeDetailLayout.js
@@ -79,33 +79,7 @@ class RecipeDetailLayout extends React.Component {
         );
     }
 }
-{/* <img src="https://res.cloudinary.com/hksqkdlah/image/upload/s--l2y15uPK--/c_scale,f_auto,h_688,q_jpegmini:2,w_688/24617_sfs-paella-for-two-10" />
 
-<Typography paragraph variant="title">
-    Method:
-  </Typography>
-<Typography paragraph variant="body">
-    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-    minutes.
-  </Typography>
-<Typography paragraph variant="body">
-    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-    chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-    salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-    minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-  </Typography>
-<Typography paragraph variant="body">
-    Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-    cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-    Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-    the rice, and cook again without stirring, until mussels have opened and rice is
-    just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-  </Typography>
-<Typography variant="body">
-    Set aside off of the heat to let rest for 10 minutes, and then serve.
-  </Typography> */}
 RecipeDetailLayout.propTypes = {
     open: PropTypes.bool,
     close: PropTypes.func,
